fec9877de7cdcc0bd99a8b66d38140f52b803f2f
MAYANK AGARWAL <63557923+mayankag30@users.noreply.github.com>
2021-07-09 03:09:28 +0530

    All the files committed

---
+++ app.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
+const connectDB = require("./config/db");
+
+// To load the config file
+dotenv.config({ path: "./config/config.env" });
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Start DB
+connectDB();
+
+const app = express();
+
+// Body Parser - to accept form data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Method Override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+// It shows: HTTP methods, Response, etc. on the console.
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
+
+// Handlebars - Setup VIEW ENGINE
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false, // Don't create session until anything is stored
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set Global Variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
+);
